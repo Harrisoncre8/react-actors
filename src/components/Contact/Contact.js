2eb42e192b2de6d42a95aef3cdc4246075d8931a
Harrison Nguyen <nguy2173@umn.edu>
2019-12-02 16:52:59 -0600

    Form made in Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,6 +6,12 @@ class Contact extends Component {
         return(
             <>
             <h2>Contact</h2>
+            <form>
+                <input placeholder="First Name"></input>
+                <input placeholder="Last Name"></input>
+                <input placeholder="Best Impression"></input>
+                <button>Join Us!</button>
+            </form>
                 <p>Phone: 1-800-WAT-WORK</p>
                 <p>Email: frame@works.io</p>
             </>
