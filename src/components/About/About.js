b52da1ecf0f1e8583d14974c7d0168420a6c189f
Harrison Nguyen <nguy2173@umn.edu>
2019-12-02 16:32:58 -0600

    Styling for footer and contact

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,10 +6,10 @@ class About extends Component {
         return(
             <>
                 <h2>About</h2>
-                <p>We are actors working with actors and we react to things.
-                    We hope to have an angular vue into the process of making a star.
-                    Will we fizz out like the ember of a small flame or catch like a
-                    giant meteor? Only a true artist with the backbone of a thespian
+                <p>We are actors working with actors and we react to things.<br></br>
+                    We hope to have an angular vue into the process of making a star.<br></br>
+                    Will we fizz out like the ember of a small flame or catch like a<br></br>
+                    giant meteor? Only a true artist with the backbone of a thespian<br></br>
                     shall have this node to joy.
                 </p>
             </>
