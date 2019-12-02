82e772950b14ecd0aa19fe8884088b2d2ef5eaa2
Harrison Nguyen <nguy2173@umn.edu>
2019-12-02 16:20:33 -0600

    Added Contact components

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,1 +1,16 @@
 import React, { Component } from 'react';
+
+class Contact extends Component {
+
+    render(){
+        return(
+            <>
+            <h2>Contact</h2>
+                <p>Phone: 1-800-WAT-WORK</p>
+                <p>Email: frame@works.io</p>
+            </>
+        )
+    }
+}
+
+export default Contact;
