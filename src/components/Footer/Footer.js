9a98b4e951f677f61d19c3e092fb0e5da9b9621f
Harrison Nguyen <nguy2173@umn.edu>
2019-12-02 16:22:18 -0600

    Added Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,1 +1,12 @@
 import React, { Component } from 'react';
+
+class Footer extends Component {
+
+    render(){
+        return(
+            <footer>© React Actors </footer>
+        )
+    }
+}
+
+export default Footer;
