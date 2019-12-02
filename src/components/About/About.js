08dd8f7aeb5c06caedd56ae16bfddb7a23986317
Harrison Nguyen <nguy2173@umn.edu>
2019-12-02 16:13:16 -0600

    Added About components

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,1 +1,21 @@
 import React, { Component } from 'react';
+
+class About extends Component {
+
+    render(){
+        return(
+            <>
+                <h2>About</h2>
+                <p>We are actors working with actors and we react to things.
+                    We hope to have an angular vue into the process of making a star.
+                    Will we fizz out like the ember of a small flame or catch like a
+                    giant meteor? Only a true artist with the backbone of a thespian
+                    shall have this node to joy.
+                </p>
+            </>
+            
+        )
+    }
+}
+
+export default About;
